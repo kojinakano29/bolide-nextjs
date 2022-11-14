c8e317c3b72761e5edb6569a4ed81ee687ef5220
KojiNakano <koji.nakano@ai-communication.jp>
2022-11-15 01:00:14 +0900

    500

---
+++ src/pages/dellamall/admin/shop/comment/[id].js
@@ -0,0 +1,85 @@
+import styles from '@/styles/dellamall/components/adminList.module.scss'
+import PageLayoutDellamall from "@/components/Layouts/PageLayoutDellamall";
+import Container from '@/components/dellamall/Layouts/container';
+import { useAuth } from '@/hooks/auth';
+import axios from '@/lib/axios';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { Btn01 } from '@/components/dellamall';
+
+export const getServerSideProps = async ({params}) => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_DELLAMALL}/shop/official_comments/${params.id}`)
+  const data = await res.json()
+
+  return {
+      props: {
+          posts: data
+      }
+  }
+}
+
+const CommentList = ({posts}) => {
+  // console.log(posts)
+  const csrf = () => axios.get('/sanctum/csrf-cookie')
+
+  const router = useRouter()
+  const { user } = useAuth({middleware: 'auth'})
+
+  const onLoadCheck = async () => {
+    if (user?.account_type === 0) {
+      router.push('/dellamall/')
+    }
+  }
+
+  useEffect(() => {
+    if (user) {
+      onLoadCheck()
+    }
+  }, [user])
+
+  const handleClickDelete = async () => {
+    await csrf()
+
+    await axios.delete(`/api/dellamall/comment/delete`, {
+      data: {
+        user_id: user?.id,
+        comment_id: router.query.id,
+      }
+    }).then((res) => {
+      // console.log(res)
+      alert("コメントを削除しました。")
+      router.reload()
+    }).catch((e) => {
+      console.error(e)
+    })
+  }
+
+  return (
+    <section className="cont1">
+      <Container small>
+        <h2 className="ttl2">コメント一覧</h2>
+        <article className={styles.listBox}>
+          {posts.map((comment, index) => (
+            <div className={styles.list} key={index}>
+              <p className={styles.content}>{comment.content}</p>
+              <div className={styles.btnBox}>
+                <button
+                  type="button"
+                  className={styles.delete}
+                  onClick={handleClickDelete}
+                >削除</button>
+              </div>
+            </div>
+          ))}
+        </article>
+        <Btn01 txt="ショップ一覧へ戻る" link="/dellamall/admin/shop" />
+      </Container>
+    </section>
+  );
+}
+
+export default CommentList;
+
+CommentList.getLayout = function getLayout(page) {
+  return <PageLayoutDellamall>{page}</PageLayoutDellamall>
+}
--- src/pages/dellamall/admin/shop/index.js
+++ src/pages/dellamall/admin/shop/index.js
@@ -1,11 +1,119 @@
+import styles from '@/styles/dellamall/components/adminList.module.scss'
 import Container from "@/components/dellamall/Layouts/container";
 import PageLayoutDellamall from "@/components/Layouts/PageLayoutDellamall";
+import { useAuth } from "@/hooks/auth";
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import axios from '@/lib/axios';
+import { Loader } from '@/components/dellamall';
 
 const AdminShop = () => {
+  const csrf = () => axios.get('/sanctum/csrf-cookie')
+
+  const { user } = useAuth({middleware: 'auth'})
+  const [createShop, setCreateShop] = useState([])
+  const [processing, setProcessing] = useState(false)
+
+  const onLoadList = async () => {
+    await setProcessing(true)
+    await csrf()
+
+    await axios.post(`/api/dellamall/user/create_shop`, {
+      user_id: user?.id,
+    }).then((res) => {
+      // console.log(res)
+      setCreateShop(res.data)
+    }).catch((e) => {
+      console.error(e)
+    })
+
+    await setProcessing(false)
+  }
+
+  useEffect(() => {
+    onLoadList()
+  }, [user])
+
+  const handleClickDelete = async (id) => {
+    if (processing) return
+    await setProcessing(true)
+    await csrf()
+
+    await axios.delete(`/api/dellamall/shop/delete/${id}`)
+    .then((res) => {
+      console.log(res)
+      onLoadList()
+      alert("ショップを削除しました。")
+    }).catch((e) => {
+      console.error(e)
+    })
+
+    await setProcessing(false)
+  }
+
+  // const [check, setCheck] = useState(false)
+
+  // const handleClickCheck = (e) => {
+  //   setCheck(e.target.checked)
+  // }
+
   return (
     <section className="cont1">
       <Container small>
         <h2 className="ttl2">作成したショップ一覧</h2>
+        {user && !processing ?
+          <article className={styles.listBox}>
+            {createShop?.map((shop, index) => (
+              <div className={styles.list} key={index}>
+                <Link href={`/dellamall/shop/${shop.id}`}>
+                  <a className={styles.link}>{shop.name}</a>
+                </Link>
+                <div className={styles.btnBox}>
+                  <Link href={`/dellamall/admin/shop/edit/${shop.id}`}>
+                    <a className={`${styles.btn} hoverEffect`}>基本情報</a>
+                  </Link>
+                  {shop.official_user_id ?
+                    <>
+                      <Link href={`/dellamall/admin/shop/official/${shop.id}`}>
+                        <a className={`${styles.btn} hoverEffect`}>公式情報</a>
+                      </Link>
+                      <Link href={`/dellamall/admin/shop/comment/${shop.id}`}>
+                        <a className={`${styles.btn} hoverEffect`}>コメント</a>
+                      </Link>
+                    </>
+                  : null}
+                  <button
+                    type="button"
+                    className={styles.delete}
+                    onClick={() => handleClickDelete(shop.id)}
+                  >削除</button>
+                </div>
+              </div>
+            ))}
+          </article>
+        : <Loader />}
+
+
+
+
+
+        {/* <dl>
+          <dt>
+            <label htmlFor="permission">キャプチャの表示・非表示</label>
+          </dt>
+          <dd>
+            <label htmlFor="permission" className={`${styles.checkBtn} ${check ? styles.on : null}`}>
+              <span>表示</span>
+              <span>非表示</span>
+              <input
+                type="checkbox"
+                id="permission"
+                {...register("image_permission")}
+                onClick={handleClickCheck}
+              />
+            </label>
+          </dd>
+        </dl> */}
       </Container>
     </section>
   );
