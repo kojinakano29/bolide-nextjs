2512322acabba96e67c3639d6641d9d66abc5dd1
KojiNakano <koji.nakano@ai-communication.jp>
2022-12-19 22:33:41 +0900

    1219

--- src/components/corapura/Loader.js
+++ src/components/corapura/Loader.js
@@ -1,4 +1,4 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faSpinner} from '@fortawesome/free-solid-svg-icons'
 
 const Loader = () => {
