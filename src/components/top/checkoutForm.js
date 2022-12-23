73f9cc82db3a2609448a5721b512894f1e043959
KojiNakano <koji.nakano@ai-communication.jp>
2022-12-23 17:00:59 +0900

    1223

--- src/components/top/checkoutForm.js
+++ src/components/top/checkoutForm.js
@@ -1,13 +1,65 @@
-import { PaymentElement } from "@stripe/react-stripe-js";
+import axios from '@/lib/axios';
 import styles from '@/styles/top/components/form.module.scss'
+import { PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from 'react';
 
-const CheckoutForm = () => {
+const CheckoutForm = ({user}) => {
+  const csrf = () => axios.get('/sanctum/csrf-cookie')
+
+  const [disabled, setDisabled] = useState(false)
+  const [name, setName] = useState("")
+  const elements = useElements()
+  const stripe = useStripe()
+
+  const onSuccessStripe = async (pm) => {
+    await csrf()
+
+    await axios.post(`/api/subscription/subscribe/${user?.id}`, {
+      payment_method: pm,
+      db_name: "corporate",
+      plan: "price_1MH4p7LPvAJPNlRsnSq9a5MR",
+    }).then((res) => {
+      console.log(res)
+    }).catch(e => console.error(e))
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setDisabled(true)
+
+    if (!stripe || !elements) return
+
+    await stripe.confirmSetup({
+      elements,
+      redirect: "if_required",
+      confirmParams: {
+        return_url: process.env.NEXT_PUBLIC_FRONTEND_URL
+      },
+    }).then((res) => {
+      console.log(res)
+      onSuccessStripe(res.setupIntent.payment_method)
+    })
+
+    await setDisabled(false)
+  }
+
   return (
-    <form>
-      <PaymentElement />
-      <button>送信する</button>
-    </form>
-  );
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          カード名義人
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="TARO YAMADA"
+          />
+        </label>
+        <PaymentElement />
+        <button disabled={disabled}>送信する</button>
+      </form>
+    </div>
+  )
 }
 
 export default CheckoutForm;
--- src/pages/payment/[id].js
+++ src/pages/payment/[id].js
@@ -1,7 +1,9 @@
-import styles from '@/styles/top/components/form.module.scss'
 import { CheckoutForm } from '@/components/top';
+import { useAuth } from '@/hooks/auth';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export const getServerSideProps = async ({params}) => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/subscription/${params.id}`)
@@ -17,99 +19,33 @@ export const getServerSideProps = async ({params}) => {
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE)
 
 const Payment = ({posts}) => {
-  console.log(posts)
+  // console.log(posts)
+
+  const router = useRouter()
+  const { user } = useAuth({middleware: 'auth', type: 'register'})
+
+  const loginCheck = async () => {
+    alert("このページの閲覧権限がありません。")
+    router.push({
+      pathname: '/'
+    })
+  }
+
+  useEffect(() => {
+    if (user && parseInt(user?.id) !== parseInt(router.query.id)) {
+      loginCheck()
+    }
+  }, [user])
 
   const options = {
-    clientSecret: posts.client_secret,
+    clientSecret: posts.intent.client_secret
   }
 
   return (
     <Elements stripe={stripePromise} options={options}>
-      <CheckoutForm />
+      <CheckoutForm user={user} />
     </Elements>
   );
-
-  // const csrf = () => axios.get('/sanctum/csrf-cookie')
-
-  // const { register, handleSubmit, formState: { errors } } = useForm({
-  //   mode: 'onChange',
-  // })
-
-  // const onSubmit = useCallback(async (data) => {
-  //   // console.log(data)
-  //   await csrf()
-
-  //   await axios.post(`/api`, {
-  //     card_num: data.card_num,
-  //     card_limit: `${data.limit_month}/${data.limit_year}`,
-  //     card_name: data.card_name,
-  //     card_code: data.card_code,
-  //   }).then((res) => {
-  //     console.log(res)
-  //   }).catch(e => console.error(e))
-  // }, [])
-
-  // return (
-  //   <section className={styles.paymentForm}>
-  //     <h2>カード情報入力画面</h2>
-  //     <form onSubmit={handleSubmit(onSubmit)}>
-  //       <dl>
-  //         <dt>
-  //           <label htmlFor="card_num">カード情報</label>
-  //         </dt>
-  //         <dd>
-  //           <input
-  //             type="text"
-  //             id="card_num"
-  //             {...register("card_num", {required: true})}
-  //             placeholder="0000000000000000"
-  //           />
-  //           {errors.card_num && <p className={styles.error}>必須項目を入力してください</p>}
-  //         </dd>
-  //       </dl>
-  //       <dl>
-  //         <dt>有効期限</dt>
-  //         <dd>
-  //           <select {...register("limit_month")}>
-  //             <option value="7">07</option>
-  //           </select>
-  //           <select {...register("limit_year")}>
-  //             <option value="2025">2025</option>
-  //           </select>
-  //         </dd>
-  //       </dl>
-  //       <dl>
-  //         <dt>
-  //           <label htmlFor="card_name">カード名義</label>
-  //         </dt>
-  //         <dd>
-  //           <input
-  //             type="text"
-  //             id="card_name"
-  //             {...register("card_name", {required: true})}
-  //             placeholder="TARO YAMADA"
-  //           />
-  //           {errors.card_name && <p className={styles.error}>必須項目を入力してください</p>}
-  //         </dd>
-  //       </dl>
-  //       <dl>
-  //         <dt>
-  //           <label htmlFor="card_code">セキュリティコード</label>
-  //         </dt>
-  //         <dd>
-  //           <input
-  //             type="text"
-  //             id="card_code"
-  //             {...register("card_code", {required: true})}
-  //             placeholder="000"
-  //           />
-  //           {errors.card_code && <p className={styles.error}>必須項目を入力してください</p>}
-  //         </dd>
-  //       </dl>
-  //       <button>送信する</button>
-  //     </form>
-  //   </section>
-  // );
 }
 
 export default Payment;
